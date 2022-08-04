f065ec23966216305730867e86c20af5af02b7e8
Annie Chen <annie@mercury.com>
2022-08-04 16:52:09 -0400

    Refactor to use better haskell practices! And clean up code

--- src/LogAnalysis.hs
+++ src/LogAnalysis.hs
@@ -7,37 +7,35 @@ import Debug.Trace
 checkMessageType :: String -> Maybe MessageType
 checkMessageType ('I':_) = Just Info
 checkMessageType ('W':_) = Just Warning
-checkMessageType ('E':xs) = let errorArray = drop 1 (splitOn " " xs) -- drop empty string 
-  in Just (Error (read (head errorArray)))
+checkMessageType ('E':xs) = Just $ Error $ read $ head $ words xs
 checkMessageType _ = Nothing
 
-getTimeStamp :: String -> TimeStamp
-getTimeStamp ('I':xs) = extractTimeStampFromInfoOrWarning xs 
-getTimeStamp ('W':xs) = extractTimeStampFromInfoOrWarning xs
-getTimeStamp ('E':xs) = extractTimeStampFromError xs 
+getTimeStamp :: String -> Maybe TimeStamp
+getTimeStamp ('I':xs) = Just $ extractTimeStampFromInfoOrWarning xs 
+getTimeStamp ('W':xs) = Just $ extractTimeStampFromInfoOrWarning xs
+getTimeStamp ('E':xs) = Just $extractTimeStampFromError xs 
+getTimeStamp _ = Nothing
 
 extractTimeStampFromInfoOrWarning :: String -> TimeStamp
-extractTimeStampFromInfoOrWarning line = read (head (drop 1 (splitOn " " line)))
+extractTimeStampFromInfoOrWarning line = read (head $ words line)::Int
+
+extractTimeStampFromError :: String -> TimeStamp
+extractTimeStampFromError line = read (head $ drop 1 $ words line)::Int
 
 getString :: String -> String
 getString line 
-  | head line == 'I' || head line == 'W' = concatStringsInArrayWithSpace (drop 2 (splitOn " " line))
-  | head line == 'E' = concatStringsInArrayWithSpace (drop 3 (splitOn " " line))
-  | otherwise = line 
-
-concatStringsInArrayWithSpace :: [String] -> String
-concatStringsInArrayWithSpace xs = drop 1 (foldl (\a b -> (++)((++) a " ") b) "" xs)
-
-extractTimeStampFromError :: String -> TimeStamp
-extractTimeStampFromError line = read (head (drop 2 (splitOn " " line)))
-
+  | head line == 'I' || head line == 'W' = unwords $ drop 2 $ words line
+  | head line == 'E' = unwords $ drop 3 $ words line
+  | otherwise = line
+ 
 -- Parse one log message
 parseMessage :: String -> LogMessage
-parseMessage line = let msgType = checkMessageType line
-  in 
-    case msgType of
-      Just t -> LogMessage t (getTimeStamp line) (getString line) 
-      Nothing -> Unknown (getString line)
+parseMessage line =
+  case checkMessageType line of
+    Just msgType -> case getTimeStamp line of
+                Just ts -> LogMessage msgType ts (getString line)
+                Nothing -> Unknown (getString line)
+    Nothing -> Unknown (getString line)
 
 -- Parse entire log file
 parse :: String -> [LogMessage]
